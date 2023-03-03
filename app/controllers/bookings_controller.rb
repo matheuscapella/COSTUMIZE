82e61d35ddc590a1c12f1961c5d7002d0454fd26
Manna Le <riverle@hotmail.com>
2023-03-03 10:15:24 -0800

    homepage-styling

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,32 @@
 class BookingsController < ApplicationController
 
-  def show
-    @booking = Booking.new
-  end
+  # def show
+  #   @booking = Booking.new
+  # end
 
   def update
     @booking = Booking.find(params[:id])
-    @booking.update(params[:booking])
+    if @booking.update(booking_params)
+      # redirect_to # up to you...
+    else
+      # render # where was the booking update form?
+    end
+  end
+
+  # def approve
+  #   @booking = Booking.find_by_id(params[:id])
+  #    @booking.update(state: "approved")
+  #    if @booking.state == "approved"
+  #      flash[:success] = "Booking successfully approved"
+  #      redirect_to bookings_path
+  #    else
+  #      flash[:error] = "Booking not approved"
+  #      redirect_to bookings_path
+  #    end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status, :start_date, :end_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :pages
-  resources :costumes, only: [:index, :new, :create, :show]
+  resources :costumes, only: [:index, :new, :create, :show] do
+    resources :bookings
+  end
 end
