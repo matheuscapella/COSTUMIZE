cecc07a21e79da10d6fddaf7ce6c4195f1a14b47
Manna Le <riverle@hotmail.com>
2023-03-03 12:02:27 -0800

    add dotenv-rails

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -7,6 +7,7 @@ class CostumesController < ApplicationController
 
   def show
     @costume = Costume.find(params[:id])
+    @booking = Booking.new
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
 user = User.create(name: "Luke", email: "Luke@gmail.com", password: "123456")
-costume = Costume.create(price: 100, description: "Clown", user: user)
+costume = Costume.new(price: 100, title: "Clown", description: "Laughing in color", user: user)
+costume.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+costume.save
+# https://res.cloudinary.com/dvjbqqvo9/image/upload/v1677783188/ben-wicks-Ns96HhR-b-c-unsplash_gejptx.jpg
