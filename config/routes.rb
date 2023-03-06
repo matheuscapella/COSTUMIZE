8ee05907613b18957be3f3aa72113207929b033c
Manna Le <riverle@hotmail.com>
2023-03-06 12:43:39 -0800

    links

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :pages
-  resources :costumes, only: [:index, :new, :create, :show] do
+  resources :costumes, only: [:index, :new, :create, :show, :destroy] do
     resources :bookings, only: [:new, :create]
   end
+    resources :bookings, only: [:show]
 end
