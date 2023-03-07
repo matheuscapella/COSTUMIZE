edb882488f231fbb714428b07f347a608e9cf08b
joaoarthurvalle <joaoarthur_976@hotmail.com>
2023-03-07 12:31:20 -0300

    Searchbar

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -3,4 +3,10 @@ class Costume < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
 
+  include PgSearch::Model
+  pg_search_scope :search_by_title_and_description,
+    against: [ :title, :description ],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
